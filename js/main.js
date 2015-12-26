00e80f34e32b270835d6c31c8199aad5699a9c94
Tomi Novak <tominovak33@gmail.com>
2015-12-26 06:26:20 +0000

    change layour

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+// Load email address
+var emailLink = document.getElementById("emailLink");
+emailLink.href = "mailto:tomi@tomi33.co.uk";
+emailLink.innerHTML = 'tomi@tomi33.co.uk';
+
+var aboutMeLink = document.getElementById("aboutMeLink");
+aboutMeLink.addEventListener("click", function(){
+    tomiScroll("about", 800);
+});
+
+function tomiScroll(targetID, scrollDuration) {
+    var page = $('html,body');
+    var element_to_scroll_to = $("#"+targetID);
+    page.animate({
+        scrollTop: element_to_scroll_to.offset().top
+    }, scrollDuration);
+}
+
 
